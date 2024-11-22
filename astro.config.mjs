77e6ee16dde7fc171b3a6154b1f36c96179e1bf6
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2024-11-22 05:18:49 -0500

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import icon from "astro-icon";
+import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: netlify(),
+  output: 'server',
+  adapter: netlify(),
+  integrations: [tailwind(), icon(), mdx(), react(), sitemap(), astroI18next() ],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'hero-pattern': "url('/src/assets/img/br_front_1.jpg')",
+				'hero-pattern-02': "url('/src/assets/img/br_front_2.jpg')",
+				'hero-pattern-03': "url('/src/assets/img/br_front_3.png')",
+				
+			  },
+			fontFamily: {
+				display: ["Oswald"],
+				body: ['"Roboto Slab"'],
+				heading: ["Vidaloka"],
+				hero: ["Roboto"],
+				pattern: ["Montserrat"],
+     			
+			  },
+			  animation: {
+				marquee: 'marquee 25s linear infinite',
+			  },
+			  keyframes: {
+				marquee: {
+				  '0%': { transform: 'translateX(0%)' },
+				  '100%': { transform: 'translateX(-100%)' },
+				}}
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'), require("@tailwindcss/typography"), require('tailwindcss-motion')
+	],
+}
